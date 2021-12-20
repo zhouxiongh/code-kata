866a612c04c5f7d075e356b7c1789ad64b34f273
hezhoux <hezhoux@qq.com>
2021-12-20 19:15:41 +0800

    Create kata18.py

---
+++ kata18.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    kata18.py
+# @Author:      jason
+# @Time:        2021/12/20 18:33
+
+""" 
+Kata18: Transitive Dependencies http://codekata.com/kata/kata18-transitive-dependencies/
+"""
+from typing import List
+import collections
+
+
+class Dependencies:
+    def __init__(self):
+        self._dependencies = collections.defaultdict(list)
+
+    def add_direct(self, u, v):
+        for dependency in v:
+            if dependency not in self._dependencies[u]:
+                self._dependencies[u].append(dependency)
+
+    def dependencies_for(self, u) -> List[str]:
+        ans = []
+        for dependency in self._dependencies[u]:
+            if dependency not in ans:
+                ans.append(dependency)
+                if dependency in self._dependencies:
+                    ans.extend(self.dependencies_for(dependency))
+
+        return sorted(ans)
