36c707d76945aaaf9e53b4cacf55bcf4c217f713
hezhoux <hezhoux@qq.com>
2021-12-21 18:55:09 +0800

    Create kata20.py

---
+++ kata20.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    kata20.py
+# @Author:      jason
+# @Time:        2021/12/21 17:36
+
+"""
+Kata20: Klondike http://codekata.com/kata/kata20-klondike/
+wiki https://en.wikipedia.org/wiki/Klondike_(solitaire)
+"""
+
+# part 1
+class Klondike:
+    pass
+
+
+# part 2
+def strategies():
+    pass
