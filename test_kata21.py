fa020ae4eb2f42d7c7e2bfde7c5783d5f7c3fd8d
hezhoux <hezhoux@qq.com>
2021-12-21 18:55:07 +0800

    Create test_kata21.py

---
+++ test_kata21.py
@@ -0,0 +1,15 @@
+import unittest
+
+from kata21 import SinglyLinkedList, DoublyLinkedList
+
+
+class TestCase(unittest.TestCase):
+    def test_singly(self):
+        list1 = SinglyLinkedList()
+
+    def test_doubly(self):
+        list1 = DoublyLinkedList()
+
+
+if __name__ == "__main__":
+    unittest.main()
